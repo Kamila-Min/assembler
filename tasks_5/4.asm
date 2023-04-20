6da4eb268e44fd232c541305c69f37ac729557ce
Kamila-Min <minakhmetova.kr@phystech.edu>
2023-04-20 23:02:09 +0300

    Create 4.asm

---
+++ tasks_5/4.asm
@@ -0,0 +1,20 @@
+%include "st_io.inc"
+
+section .data
+a dw 250
+b dw 255
+
+global _start
+
+section .text
+_start:
+
+mov ax,  word[b]
+add word[a], ax
+mov ax, word[b+2]
+adc word[a+2], ax
+
+UNSINT [a] 
+PUTCHAR 0xa
+
+FINISH
