399aa3c36668991d57b710a5a96fa7d7a69e7e62
Kamila-Min <minakhmetova.kr@phystech.edu>
2023-04-21 00:01:05 +0300

    add file

---
+++ tasks_8/2.asm
@@ -0,0 +1,49 @@
+global _start
+
+section .bss
+c resb 20
+
+section .text
+_start:
+
+mov eax, l
+
+mov eax, 3
+mov ebx, 2
+mov ecx, c
+mov edx, 19
+
+int 0x80
+
+mov eax, 4
+mov ebx, 1
+mov ecx, c
+mov edx, 19
+
+int 0x80
+
+mov eax, 1
+mov ebx, 0
+
+int 0x80
+
+l: mov eax, 3
+mov ebx, 2
+mov ecx, c
+mov edx, 19
+
+int 0x80
+
+mov eax, 4
+mov ebx, 1
+mov ecx, c
+mov edx, 19
+
+int 0x80
+
+mov eax, 1
+mov ebx, 0
+
+int 0x80
+
+jmp eax
